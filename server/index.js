d1f49dccebff408fb210792beffe59ea298d6b0b
mefe1903 <mefe1903@mefe1903-K52JT.server1.com>
2019-05-26 02:32:14 +0300

    Initial commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const cors=require('cors');
 const bodyParser=require('body-parser');
 
-const handle=require('./handlers/index');
+const handle=require('./handlers');
 
 const app=express();
 const port=process.env.PORT;
