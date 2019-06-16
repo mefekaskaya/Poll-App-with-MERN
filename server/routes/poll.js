200137e0170febd0c1fcec1d669d2d8946652250
mefekaskaya <mefekaskaya@github.com>
2019-06-16 21:11:57 +0300

    react setup

--- server/routes/poll.js
+++ server/routes/poll.js
@@ -3,7 +3,7 @@ const handle=require('../handlers');
 const auth=require('../middlewares/auth');
 
 router.route('/').get(handle.showPolls).post(auth,handle.createPoll);
-router.get('/user',auth,handle.usersPolls);
+router.get('/user', auth, handle.userPolls);
 router.route(':id').get(handle.getPoll).post(handle.vote).delete(auth,handle.deletePoll);
 
 module.exports=router;
